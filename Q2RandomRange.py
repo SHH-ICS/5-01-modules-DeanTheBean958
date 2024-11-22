ac818dc08f8239016c664bd82374e8b8d9f9654d
Dean <dean.pauls@stu.ocsb.ca>
2024-11-22 15:43:56 +0000

    done

--- Q2RandomRange.py
+++ Q2RandomRange.py
@@ -1,2 +1,6 @@
 # Create a program that accepts 2 numbers from the user. 
 # Your program will output a random number between the range given by the user.
+import random
+A = int (input("please enter a number: "))
+B = int (input("please enter a number: "))
+print(random.randint(min(A,B),max(A,B)))
--- Q4Picture.py
+++ Q4Picture.py
@@ -1,2 +1,23 @@
 # Using the pygame library, draw a simple picture. 
 # It can be anything you like, but you must use at least 2 different types of shapes and 3 different colors.
+import pygame, sys
+from pygame import Color
+from pygame import draw, Rect
+from pygame.locals import QUIT
+
+pygame.init()
+Surface = pygame.display.set_mode((600, 300))
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+    pygame.display.update()
+    Surface.fill("Lightblue")
+    
+    draw.rect(Surface, Color('darkgreen'), Rect(0, 200, 500, 200))
+
+    draw.rect(Surface, Color('brown'), Rect(100, 50, 300, 150))
+
+
+    draw.circle(Surface, Color('yellow'), (50, 50), 50)
